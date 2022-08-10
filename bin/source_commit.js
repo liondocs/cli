d83d1003be3a3f3f7f2dc9d5edb0b7284bc98320
Juan Vasquez <juan@ombulabs.com>
2022-08-09 22:29:23 -0500

    Add argv parser
    - [ ] Move show sha command
    - [ ] Add config command
    - [ ] Add help command, will show all available options

--- bin/source_commit.js
+++ bin/source_commit.js
@@ -1,22 +1,26 @@
 #! /usr/bin/env node
 
-let shell = require("shelljs")
-let args  = process.argv.slice(2)
+const {parser} = require("../lib/index")
+const command = parser(process.argv)
+console.log(command);
 
-let fromKey = args[0]
-let fromValue = args[1]
-
-// --from or -f key is required
-if (!["--from", "-f"].includes(fromKey)) {
-  shell.echo("--from key is required: source_commit --from /path/to/file")
-  shell.exit(1)
-}
-
-// value of --from is required
-if (fromValue === undefined) {
-  shell.echo("--from value is required: source_commit --from /path/to/file")
-  shell.exit(1)
-}
-
-let sha = shell.exec("git log --pretty=format:'%H' -n 1 " + fromValue)
-shell.echo(sha)
+// let shell = require("shelljs")
+// let args  = process.argv.slice(2)
+//
+// let fromKey = args[0]
+// let fromValue = args[1]
+//
+// // --from or -f key is required
+// if (!["--from", "-f"].includes(fromKey)) {
+//   shell.echo("--from key is required: source_commit --from /path/to/file")
+//   shell.exit(1)
+// }
+//
+// // value of --from is required
+// if (fromValue === undefined) {
+//   shell.echo("--from value is required: source_commit --from /path/to/file")
+//   shell.exit(1)
+// }
+//
+// let sha = shell.exec("git log --pretty=format:'%H' -n 1 " + fromValue)
+// shell.echo(sha)
