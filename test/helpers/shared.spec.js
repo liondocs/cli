830b08015d3cd0f30fd9391fb1f412a3025f3def
Juan Vásquez <juan@ombulabs.com>
2022-08-12 16:45:47 -0500

    Add basic tests (#24)
    * Add basic tests
    
    - [x] Configure jest
    - [x] Add basic tests
    
    Closes #5
    
    * Run test in CI

---
+++ test/helpers/shared.spec.js
@@ -0,0 +1,40 @@
+const {
+  commandNotFoundMessage,
+  helpMessage,
+  pathIsRequiredMessage
+} = require('../../lib/helpers/shared')
+
+describe('commandNotFoundMessage', () => {
+  it('returns a message', () => {
+    const expected = `
+    Command Not Found, Verify Available Commands Running:
+    source_commit --help
+  `
+
+    expect(commandNotFoundMessage()).toEqual(expected)
+  })
+})
+
+describe('helpMessage', () => {
+  it('returns a message', () => {
+    const expected = `
+    source_commit [command] [params]
+
+    --------------------------------------------------------------------
+    | Command       | Arguments        | Description                   |
+    --------------------------------------------------------------------
+    | --sha or -s   | path/to/the/file | Display the sha of given file |
+    --------------------------------------------------------------------
+    | --help or -h  |                  | Display available commands    |
+    --------------------------------------------------------------------
+  `
+
+    expect(helpMessage()).toEqual(expected)
+  })
+})
+
+describe('pathIsRequiredMessage', () => {
+  it('returns a message', () => {
+    expect(pathIsRequiredMessage()).toEqual('path is required')
+  })
+})
